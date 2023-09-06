5bb99a879955ac6248e355c95b47599c8d0e7c20
Aishvarya Ravichandran <108511587+lavenderhazel2003@users.noreply.github.com>
2023-09-06 19:26:56 +0530

    Delete src directory

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export {default as SignIn } from './SignIn';
-export {default as SignUp } from './SignUp';
-export {default as Dashboard } from './Dashboard';
-export {default as Products } from './Products';
-export {default as Cart } from './Cart';
-export {default as Modal } from './Modal';
-export {default as ModalUpdate } from './ModalUpdate';
